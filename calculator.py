6684925615d5d1a8e5681ed5d6432681e456bb3d
Morgan Grady <morgane.grady@gmail.com>
2026-09-17 19:58:40 -0400

    docs: add docstring for add method

--- calculator.py
+++ calculator.py
@@ -2,4 +2,5 @@ class Calculator:
     """Performs basic mathematical operations."""
 
     def add(self, first_number, second_number):
+        """Return the sum of two numbers."""
         return first_number + second_number
