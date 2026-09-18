3fff0accda1152df147dff834f285e186d6de006
Morgan Grady <morgane.grady@gmail.com>
2026-09-18 17:36:57 -0400

    docs: update method docstrings

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@ class Calculator:
     """Performs basic mathematical operations."""
 
     def add(self, first_number, second_number):
-        """Return the sum of two numbers"""
+        """Return the sum of two numbers."""
         return first_number + second_number
 
     def subtract(self, first_number, second_number):
-        """Return the difference of two numbers"""
+        """Return the difference of two numbers."""
         return first_number - second_number
 
     def multiply(self, first_number, second_number):
-        """Return the product of two numbers"""
+        """Return the product of two numbers."""
         return first_number * second_number
 
 def main():
