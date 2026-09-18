3d0a5baa0d333046571fb835393d487b07228983
Morgan Grady <morgane.grady@gmail.com>
2026-09-18 16:30:10 -0400

    feat: add main calculator program

--- calculator.py
+++ calculator.py
@@ -12,3 +12,36 @@ class Calculator:
     def multiply(self, first_number, second_number):
         """Return the product of two numbers"""
         return first_number * second_number
+
+def main():
+    """Runs calculator operations and handles user interaction."""
+
+    calculator = Calculator()
+
+    print("""Pick an Operation:
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    """)
+    operation = input("Enter the number for your desired operation: ")
+
+    if operation not in ("1", "2", "3"):
+        print("Selection is invalid")
+        return
+
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the second number: "))
+
+    match operation:
+        case "1":
+            result = calculator.add(first_number, second_number)
+            print(f"The sum of {first_number} + {second_number} is {result}")
+        case "2":
+            result = calculator.subtract(first_number, second_number)
+            print(f"The difference of {first_number} - {second_number} is {result}")
+        case "3":
+            result = calculator.multiply(first_number, second_number)
+            print(f"The product of {first_number} * {second_number} is {result}")
+
+
+main()
