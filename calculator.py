6f946365e5ed557007eef1a4c8c13fe7ccca4b61
Morgan Grady <morgane.grady@gmail.com>
2026-09-17 20:04:07 -0400

    feat: add subtraction operation

--- calculator.py
+++ calculator.py
@@ -2,5 +2,9 @@ class Calculator:
     """Performs basic mathematical operations."""
 
     def add(self, first_number, second_number):
-        """Return the sum of two numbers."""
+        """Return the sum of two numbers"""
         return first_number + second_number
+
+    def subtract(self, first_number, second_number):
+        """Return the difference of two numbers"""
+        return first_number - second_number
